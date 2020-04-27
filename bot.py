63a55ac057e327435555825d62b900a74cdd8740
Alexey Shishkin <dr.alexa2000@mail.ru>
2020-04-27 13:35:27 +0300

    added different messages

--- bot.py
+++ bot.py
@@ -2,16 +2,33 @@ from telegram.ext import Updater, CommandHandler
 from logic import Match, Tournament
 import time
 import shelve
+import random
 
 
 currentTournament = Tournament()
 current_emoji = ":dice"
 
+test_messages = ['Проверка связи', 'Ало, ало, как слышно']
+success_start_tournament_messages = ['Турнир начался', 'Давайте-ка начнем играть']
+tournament_is_running_messages = ['Турнир идёт', 'Существует активный турнир']
+begin_registration_messages = ['Турнир завершен, открывается регистрация на новый', 'Ожидаем желающих посоревноваться']
+success_registration_messages = ['Успешная регистрация на этот замечательный турнир', 'Проходите, присаживайтесь']
+few_participants_messages = ['Турнир не может быть начат, минимум 2 участника', 'Мало игроков, ждём-с']
+no_registrations_messages = ['Нет зарегистрировавшихся', 'Никто не хочет играть(']
+list_registrations_messages = ['Список зарегистрировавшихся: ']
+statistics_messages = ['Статистика:']
+draw_messages = ['Ого, похоже на ничью, переигровка!', 'Опять админ подкручивает, давайте заново']
+match_winner_messages = ['И побеждает', 'Победитель матча']
+not_your_turn_messages = ['Эм, но вы не участвуете в текущем матче, либо уже сделали ход', 'Извините, сейчас не ваш ход']
+not_started_tournament_messages = ['Турнир еще не начался', 'Рано, нет действующих турниров']
+match_notify_messages = ['Матч между', 'Объявляется противостояние ']
+tournament_winner_messages = ['Победитель турнира:', 'Поздравляем, ']
+
 
 def hard_reset(bot, chat_id):
     global currentTournament
     currentTournament = Tournament()
-    bot.sendMessage(chat_id, 'Турнир завершен, открывается регистрация на новый')
+    bot.sendMessage(chat_id, random.choice(begin_registration_messages))
 
 
 def reset(bot, update):
@@ -19,14 +36,14 @@ def reset(bot, update):
 
 
 def test(bot, update):
-    update.message.reply_text('Проверка связи')
+    update.message.reply_text(random.choice(test_messages))
 
 
 def start_tournament(bot, update, args):
     global currentTournament
     global current_emoji
     if currentTournament.isStarted():
-        update.message.reply_text('Турнир идёт')
+        update.message.reply_text(random.choice(tournament_is_running_messages))
     elif currentTournament.canBeStarted():
         if len(args) == 1 and args[0] == "darts":
             current_emoji = ":dart:"  # Here dart emoji
@@ -34,28 +51,28 @@ def start_tournament(bot, update, args):
             current_emoji = ":dice:"  # Here dice emoji
         currentTournament.start()
         chat_id = update.message.chat.id
-        bot.sendMessage(chat_id, 'Турнир начался')
+        bot.sendMessage(chat_id, random.choice(success_start_tournament_messages))
         next_match(bot, chat_id)
     else:
-        update.message.reply_text('Турнир не может быть начат, минимум 2 участника')
+        update.message.reply_text(random.choice(few_participants_messages))
 
 
 def register(bot, update):
     global currentTournament
     if not currentTournament.isStarted():
         currentTournament.register(update.message.from_user.username)
-        update.message.reply_text('Успешная регистрация на этот замечательный турнир')
+        update.message.reply_text(random.choice(success_registration_messages))
     else:
-        update.message.reply_text('Турнир идёт')
+        update.message.reply_text(random.choice(tournament_is_running_messages))
 
 
 def participants(bot, update):
     global currentTournament
     part_list = currentTournament.getParticipants()
     if len(part_list) == 0:
-        update.message.reply_text("Нет зарегистрировавшихся")
+        update.message.reply_text(random.choice(no_registrations_messages))
     else:
-        message = "Список зарегистрировавшихся: "
+        message = random.choice(list_registrations_messages)
         for u in part_list:
             message += u + ' '
         update.message.reply_text(message)
@@ -66,7 +83,7 @@ def stats(bot, update):
     with shelve.open('winners') as winners_map:
         for p in winners_map:
             answer.append(p + ': ' + str(winners_map[p]))
-    bot.sendMessage(update.message.chat.id, 'Статистика:\n' + '\n'.join(answer))
+    bot.sendMessage(update.message.chat.id, random.choice(statistics_messages) + '\n' + '\n'.join(answer))
 
 
 def throw(bot, update):
@@ -79,26 +96,26 @@ def throw(bot, update):
             number_on_dice = update.message.reply_dice(emoji = current_emoji).dice.value
             result = currentTournament.getCurrentMatch().setResult(user, number_on_dice)
             if result is not None:
-                time.sleep(4)
+                time.sleep(5)
                 if result == "!":
-                    bot.sendMessage(chat_id, "Ого, похоже на ничью, переигровка!")
+                    bot.sendMessage(chat_id, random.choice(draw_messages))
                 else:
-                    bot.sendMessage(chat_id, "Побеждает @" + result)
+                    bot.sendMessage(chat_id, random.choice(match_winner_messages) + " @" + result)
                     next_match(bot, chat_id)
         else:
-            update.message.reply_text("Эм, но вы не участвуете в текущем матче, либо уже сделали ход")
+            update.message.reply_text(random.choice(not_your_turn_messages))
     else:
-        update.message.reply_text("Турнир еще не начался")
+        update.message.reply_text(random.choice(not_started_tournament_messages))
 
 
 def next_match(bot, chat_id):
     global currentTournament
     if not currentTournament.isFinished():
         players = currentTournament.getCurrentMatch().getPlayers()
-        bot.sendMessage(chat_id, 'Матч между @' + players[0] + ' и @' + players[1])
+        bot.sendMessage(chat_id, random.choice(match_notify_messages) + ' @' + players[0] + ' и @' + players[1])
     else:
         winner = currentTournament.getWinner()
-        bot.sendMessage(chat_id, 'Победитель турнира: @' + winner)
+        bot.sendMessage(chat_id, random.choice(tournament_winner_messages) + ' @' + winner)
         with shelve.open('winners') as winners_map:
             winners_map[winner] = winners_map.get(winner, 0) + 1
         hard_reset(bot, chat_id)
