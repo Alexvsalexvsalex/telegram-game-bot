04f2834f076b4f0e3200b65b1e4a5304468d1a9c
Alexey Shishkin <dr.alexa2000@mail.ru>
2020-05-26 22:23:01 +0300

    small fixes

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ hello_messages = ["Hello, %s", "%s ආයුබෝවන්", "Բարեւ, %s", 
                  "Прывітанне %s", "Привіт %s", "Привет, %s", "Поздрав %s", "سلام به %s", "שלום %s", "Γεια σας %s",
                  "העלא %s", "ہیل%s٪ ے", "Bonjou %s", "Bonjour %s", "Bună ziua %s", "Ciao %s", "Dia duit %s",
                  "Dobrý deň %s", "Dobrý den, %s", "Habari %s", "Halló %s", "Hallo %s", "Halo %s", "Hei %s", "Hej %s",
-                 "Hello  %s", "Hello %s", "Hello %s", "Helo %s", "Hola %s", "Kaixo %s", "Kamusta %s", "Merhaba %s",
+                "Helo %s", "Hola %s", "Kaixo %s", "Kamusta %s", "Merhaba %s",
                  "Olá %s", "Ola %s", "Përshëndetje %s", "Pozdrav %s", "Pozdravljeni %s", "Salom %s", "Sawubona %s",
                  "Sveiki %s", "Tere %s", "Witaj %s", "Xin chào %s", "ສະບາຍດີ %s", "สวัสดี %s", "ഹലോ %s", "ಹಲೋ %s",
                  "హలో %s", "हॅलो %s", "नमस्कार%sको", "হ্যালো %s", "ਹੈਲੋ %s", "હેલો %s", "வணக்கம் %s",
@@ -114,7 +114,7 @@ def participants(bot, update):
 def get_text_stats(stats):
     prepared_stat = []
     for p in stats:
-        if p[2] != 0:
+        if p[3] != 0:
             prepared_stat.append([p[0], p[1], p[2], p[3], str((p[4] * 10000 // p[3]) / 100) + '%', (p[5] * 100 // p[3]) / 100])
     return '```' + tabulate(prepared_stat, headers=['NAME', 'T_P', 'T_W', 'M_W', 'W_R', 'AVG'], tablefmt="grid") + '```'
 
