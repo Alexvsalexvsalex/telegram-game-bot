4b7845a11c3762738f5b6b6e9368e8c0cb9a8345
Egor Kurbatov <avangard4700@mail.ru>
2020-05-26 20:26:36 +0300

    logic: add statistic and code-style fix

--- logic.py
+++ logic.py
@@ -1,23 +1,53 @@
 import random
 
 
+class Statistic(object):
+
+    def __init__(self):
+        self.dict_names = ["tournament_points", "number_matches", "number_wins", "sum_value"]
+        self.data = {}
+        for d in self.dict_names:
+            self.data[d] = {}
+
+    def register_throw(self, name, value):
+        self.data["number_matches"][name] += 1
+        self.data["sum_value"][name] += value
+
+    def register_win(self, name):
+        self.data["number_wins"][name] += 1
+
+    def register_tournament_winner(self, name):
+        self.data["tournament_points"][name] += self.data["number_matches"][name]
+
+    def get_full_statistic(self):
+        players = self.data["number_matches"].keys()
+        res = {}
+        for p in players:
+            res[p] = {}
+            for d in self.dict_names:
+                res[p][d] = self.data[d][p]
+        return res
+
+
 class Match(object):
 
-    def __init__(self, firstPlayer, secondPlayer):
-        self.res = {firstPlayer: None, secondPlayer: None}
+    def __init__(self, first_player, second_player, statistic):
+        self.res = {first_player: None, second_player: None}
         self.winner = None
+        self.statistic = statistic
 
-    def getPlayers(self):
+    def get_players(self):
         return list(self.res.keys())
 
-    def getWinner(self):
+    def get_winner(self):
         return self.winner
 
-    def canBeChanged(self, player):
+    def can_be_changed(self, player):
         return player in self.res and self.res[player] is None
 
-    def setResult(self, player, res):
+    def set_result(self, player, res):
         self.res[player] = res
+        self.statistic.register_throw(player, res)
         val = None
         first = None
         for p in self.res:
@@ -34,8 +64,10 @@ class Match(object):
                 return "!"
             if val < cur:
                 self.winner = p
+                self.statistic.register_win(p)
             else:
                 self.winner = first
+                self.statistic.register_win(first)
             return self.winner
 
 
@@ -46,54 +78,59 @@ class Tournament(object):
         self.participants = set()
         self.isStartedFlag = False
         self.currentMatch = None
+        self.statistic = Statistic()
 
     def register(self, player):
         self.participants.add(player)
 
-    def getParticipants(self):
+    def get_participants(self):
         return list(self.participants)
 
-    def canBeStarted(self):
+    def can_be_started(self):
         if len(self.participants) > 1:
             return True
         return False
 
-    def isStarted(self):
+    def is_started(self):
         return self.isStartedFlag
 
     def start(self):
         self.isStartedFlag = True
         leaf = 1
-        partCnt = len(self.participants)
-        partLst = list(self.participants)
-        while leaf < partCnt:
+        part_cnt = len(self.participants)
+        part_lst = list(self.participants)
+        while leaf < part_cnt:
             self.heap.append(None)
             self.heap.append(None)
             leaf = leaf + 1
-        random.shuffle(partLst)
-        for i in range(1, partCnt + 1):
-            self.heap[len(self.heap) - i] = partLst[i - 1]
-        self.generateNextMatch()
+        random.shuffle(part_lst)
+        for i in range(1, part_cnt + 1):
+            self.heap[len(self.heap) - i] = part_lst[i - 1]
+        self.generate_next_match()
 
-    def generateNextMatch(self):
-        self.currentMatch = Match(self.heap.pop(), self.heap.pop())
+    def generate_next_match(self):
+        self.currentMatch = Match(self.heap.pop(), self.heap.pop(), self.statistic)
 
-    def setWinnerOfLastMatch(self):
+    def set_winner_of_last_match(self):
         self.heap[(len(self.heap) - 2) // 2 + 1] = self.currentMatch.getWinner()
 
-    def getCurrentMatch(self):
+    def get_current_match(self):
         if self.currentMatch.getWinner() is not None:
-            self.setWinnerOfLastMatch()
-            if not self.isFinished():
-                self.generateNextMatch()
+            self.set_winner_of_last_match()
+            if not self.is_finished():
+                self.generate_next_match()
         return self.currentMatch
 
-    def isFinished(self):
+    def is_finished(self):
         if len(self.heap) == 1:
             if self.currentMatch.getWinner() is not None:
-                self.setWinnerOfLastMatch()
+                self.set_winner_of_last_match()
+                self.statistic.register_tournament_winner(self.heap[0])
                 return True
         return False
 
-    def getWinner(self):
+    def get_winner(self):
         return self.heap[0]
+
+    def get_stats(self):
+        return self.statistic.get_full_statistic()
